952e9c7e2197bec2bf0a238973dfbbb8fb68bd7f
Cynthia <cinthya3259@gmail.com>
2021-03-02 21:58:40 -0600

    create table

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,12 @@
+class PostsController < ApplicationController
+  def index
+    @posts = Post.order(created_at: :desc)
+  end
+
+  def create
+    @post = Post.create(title: params[:title], image_url: params[:image_url], content: params[:content])
+  end
+
+  def dashboard
+  end
+end
